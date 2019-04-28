72ee8589025fa2e1ea9f138c7f90e76af13bdb97
FangChang_TXCloud <hsiamer@qq.com>
2019-04-28 13:57:39 +0800

    修复了单个章节下载时没有文件后缀的问题

--- tools/mkfile.py
+++ tools/mkfile.py
@@ -8,7 +8,7 @@ def mkfile(chapterno,bookdir,chaptername,text):
     if platform.system()=='Windows':
         bookd = bookdir + '\\' + chaptername
     if platform.system()=='Linux':
-        bookd = bookdir + '/' + chaptername
+        bookd = bookdir + '/' + chaptername + '.txt'
     with open(bookd,'w+',encoding = 'utf-8') as f:
         f.write(text)
         f.close()
