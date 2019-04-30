6989db35292693a65298b130702ce3c90df19acb
FangChang_TXCloud <hsiamer@qq.com>
2019-04-30 09:31:49 +0800

    修复了书籍章节页面链接的问题

--- tools/fillchapterlist.py
+++ tools/fillchapterlist.py
@@ -1,7 +1,7 @@
 def filechapter(bookd,chaptername,bookid,chapterno):
     bookid = '0'*(6-len(str(bookid))) + bookid
     chapterno = '0'*(6-len(str(chapterno))) + str(chapterno) + ' - '
-    strs = '<a href="' + bookid + '/' + chapterno +  chaptername  + '.html" target="_blank">' + chaptername + '</a><br>\n'
+    strs = '<a href="' + bookid + '/' + chapterno + '.html" target="_blank">' + chaptername + '</a><br>\n'
     with open(bookd,'a+',encoding='utf-8') as f:
         f.write(strs)
         f.close()
